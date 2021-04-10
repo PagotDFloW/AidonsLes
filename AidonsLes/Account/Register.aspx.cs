4bbeb815b69e709ec0cd890fc07533f10162339a
Florian <dyckeuss@outlook.fr>
2021-04-10 16:08:02 +0200

    apport modification

--- AidonsLes/Account/Register.aspx.cs
+++ AidonsLes/Account/Register.aspx.cs
@@ -17,7 +17,7 @@ namespace AidonsLes.Account
         {
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            var user = new ApplicationUser() { UserName = Prenom.Text + "&nbsp;" + Nom.Text , Email = Email.Text };
+            var user = new ApplicationUser() { UserName = Prenom.Text + " " + Nom.Text , Email = Email.Text };
             IdentityResult result = manager.Create(user, Password.Text);
             if (result.Succeeded)
             {
